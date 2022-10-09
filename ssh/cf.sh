a42734604cca6fe03dbdbf18ef3434d9b3ba4965
HarmonySwift <112459623+HarmonySwift@users.noreply.github.com>
2022-10-09 22:06:03 +0700

    Update cf.sh

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -25,11 +25,11 @@ fi
 clear
 apt install jq curl -y
 ###DOM#AIN=installer.my.id
-DOMAIN=slxray.cf
+DOMAIN=arzjoanna.com
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-SUB_DOMAIN=${sub}.slxray.cf
-CF_ID=slinfinity69@gmail.com
-CF_KEY=3dcb550a77cb0a64dacf205243e3ac550f9db
+SUB_DOMAIN=${sub}.arzjoanna.com
+CF_ID=auperwing@gmail.com
+CF_KEY=e79431dc85570f38811c05f99bf44aae02501
 ##SUB_DOMAIN=${sub}.installer.my.id
 ##CF_ID=senowahyu62@gmail.com
 ##CF_KEY=84570683099becbeb0f04d8086ce6f309c56c
